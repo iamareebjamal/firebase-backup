66005185069535bd2001a15f6648257b28993acf
iamareebjamal <jamal.areeb@gmail.com>
2019-09-12 03:26:55 +0530

    feat: Allow saving on cloud

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const path = require('path');
 const {promisify} = require('util');
 const readFileAsync = promisify(fs.readFile);
 const writeFileAsync = promisify(fs.writeFile);
+const Readable = require('stream').Readable;
+const uuid = require('uuid');
 const admin = require("firebase-admin");
 
 require('dotenv').config();
@@ -22,25 +24,56 @@ async function downloadDatabase() {
     const serviceAccount = JSON.parse(await getConfig());
 
     const databaseURL = `https://${serviceAccount.project_id}.firebaseio.com`;
+    const storageBucket = `${serviceAccount.project_id}.appspot.com`;
 
     admin.initializeApp({
       credential: admin.credential.cert(serviceAccount),
-      databaseURL
+      databaseURL,
+      storageBucket
     });
     
     const db = admin.database();
     const ref = db.ref("/");
-    return ref.once("value");
+    return (await ref.once("value")).val();
+}
+
+async function saveOnDisk(fileName, content) {
+    return await writeFileAsync(path.resolve(__dirname, fileName), content);
+}
+
+async function saveOnCloud(fileName, content) {
+    const bucket = admin.storage().bucket();
+
+    await new Promise((resolve, reject) => {
+        const writeStream = bucket
+          .file(fileName)
+          .createWriteStream({
+            private: true,
+            metadata: {
+              contentType: 'text/plain',
+            },
+          })
+  
+        writeStream.on('finish', resolve).on('error', reject);
+  
+        const readStream = new Readable();
+        readStream._read = () => {}; // Workaround https://stackoverflow.com/a/22085851/3309666
+        readStream.push(content);
+        readStream.push(null);
+  
+        readStream.on('error', reject);
+        readStream.pipe(writeStream);
+      });
 }
 
 async function saveDatabase() {
-    const database = (await downloadDatabase()).val();
+    const database = await downloadDatabase();
     const serialized = JSON.stringify(database, null, 2);
 
     const date = new Date().toISOString();
-    const backupFile = `backups/backup_${ date }.json`;
+    const backupFile = `backups/backup_${ date }_${ uuid.v4() }.json`;
 
-    return writeFileAsync(path.resolve(__dirname, backupFile), serialized);
+    return await (process.env.STORE_ON_CLOUD ? saveOnCloud : saveOnDisk)(backupFile, serialized);
 }
 
 saveDatabase()
